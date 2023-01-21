c0be6d7ed31ad098480f4f333cd1557ed89231bd
Kentkn192837 <agriculture5012@gmail.com>
2023-01-21 22:39:01 +0900

    SGDとMomentum

---
+++ calculation/supervisedLearning/optimizer.py
@@ -0,0 +1,32 @@
+class SGD:
+    """
+        確率的勾配降下法
+        SGD: Stochastic Gradient Descent
+    """
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+    
+
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key]
+
+
+
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+    
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+        
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
